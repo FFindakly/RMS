f0c64859cfbacefea9a49246781bf6fa577de348
Fadi Findakly <fadi85ot@gmail.com>
2019-11-01 23:43:21 -0400

    -BorderPane added to the login.fxml -Menu with its menu items added to the BoarderPane of login screen -Functionality of exit menu item is added -Functionality of accounts settings menu item is added

--- src/sample/controllers/Login.java
+++ src/sample/controllers/Login.java
@@ -1,4 +1,31 @@
 package sample.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.MenuItem;
+import sample.Main;
+
+import java.io.IOException;
+
 public class Login {
+
+    /**
+     * function to exit from application
+     * @autor Fadi Findakly
+     * @return void
+     */
+    public void close() {
+        System.exit(1);
+    }
+
+    /**
+     * function to switch to account settings screen
+     * @autor Fadi Findakly
+     * @return void
+     */
+    public void switchToAcctSettings() throws IOException {
+            Main.setPane(FXMLLoader.load(getClass().getResource("../FXMLs/account_settings.fxml")));
+    }
+
+
 }
