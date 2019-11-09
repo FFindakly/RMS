e3a0752553f69542ec3887bb173df3b3e1fe7e27
Yonis Sheekh <yonis.sheekh01@stclairconnect.ca>
2019-11-09 05:28:11 -0500

    issue #15 added the basic update menu screen you can switch between the update screen and the create menu screens added all three basic menu screens

--- src/sample/controllers/CreateMenu.java
+++ src/sample/controllers/CreateMenu.java
@@ -1,4 +1,13 @@
 package sample.controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import sample.Main;
+
+import java.io.IOException;
+
 public class CreateMenu {
+    public void switchToShowMenu(MouseEvent event) throws IOException {
+        Main.setPane(FXMLLoader.load(getClass().getResource("../FXMLs/show_menu.fxml")));
+    }
 }
--- src/sample/controllers/ShowMenu.java
+++ src/sample/controllers/ShowMenu.java
@@ -1,5 +1,14 @@
 package sample.controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import sample.Main;
+
+import java.io.IOException;
+
 public class ShowMenu {
 
+    public void UpdateMenu(MouseEvent event) throws IOException {
+        Main.setPane(FXMLLoader.load(getClass().getResource("../FXMLs/update_menu.fxml")));
+    }
 }
