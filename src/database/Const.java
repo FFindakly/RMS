ab8556677e7f679d78b9b5ca86eaed7da8e76f01
Fadi Findakly <fadi.findakly01@stclairconnect.ca>
2019-11-17 09:54:44 -0500

    -Const class is created Categories table constants added to Const

---
+++ src/database/Const.java
@@ -0,0 +1,14 @@
+package database;
+
+/**
+ * @author Fadi Findakly
+ */
+
+public class Const {
+
+    //Create constants to represent each column in each table
+    //Categories table
+    public static final String CATEGORY_ID = "id";
+    public static final String CATEGORY_NAME = "category_name";
+
+}
